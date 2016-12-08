1ef33388ab95abc3f52a5848998719edefdb4501
Bryan Feeney <bryan.feeney@gmail.com>
2016-12-08 16:17:15 +0000

    Fix bugs in reading and add write support
    Bug: Reading of URL Card fields was attempted if they were _not_
    downloaded, just Pending
    
    Bug: returned a tuple of a tuple when reading embedded tweets or
    url-cards for a tweet due to the low precedence of boolean expressions.

--- twextender/tweet.py
+++ twextender/tweet.py
@@ -3,6 +3,8 @@ This module contains a representation of a tweet, and code for reading tweets fr
 a file written out by the Java based twitter spider.
 """
 from dateutil import parser as dateparser
+import os
+from pathlib import Path
 
 class UrlCard:
     """
@@ -28,23 +30,29 @@ class UrlCard:
         url      = fields[start + 0]
         card_url = fields[start + 1]
 
-        present  = fields[start + 2].strip().upper() == 'P'
+        present  = fields[start + 2].strip().upper() == 'D'
         if present:
-            title = fields[start + 3]
-            body  = fields[start + 4]
-            next  = start + 5
+            title   = fields[start + 3]
+            content = fields[start + 4]
+            next    = start + 5
         else:
-            title = None
-            body  = None
-            next  = start + 3
+            title   = None
+            content = None
+            next    = start + 3
 
-        return UrlCard(url, card_url, title=title, content=body), next
+        return UrlCard(url, card_url, title=title, content=content), next
+
+    def to_str_fields(self):
+        if self.title is None and self.content is None:
+            return [self.url, self.card_url, "P"]
+        else:
+            return [self.url, self.card_url, "D", self.title, self.content]
 
     def __str__(self):
         if self.title is not None:
-            return self.title + ": " + self.body + "(" + self.uri + ")"
+            return self.title + ": " + self.content + "(" + self.uri + ")"
         else:
-            return self.uri + "(detail at " + self.card_uri + ")"
+            return self.url + "(detail at " + self.card_url + ")"
 
 
 class TweetBody:
@@ -64,11 +72,21 @@ class TweetBody:
         :param embedded_tweet: if this is a quote-tweet (i.e. a retweet with commentary) then this
         is the retweet which is being commented on in the <code>content</code>
         """
-        self.tweet_id = tweet_id
+        self.tweet_id = int(tweet_id)
         self.author = author
         self.content = content
         self.embedded_url = embedded_url
         self.embedded_tweet = embedded_tweet
+
+    def to_str_fields(self):
+        core = [ self.author, str(self.tweet_id), self.content ]
+        url_card = [ "none" ] if self.embedded_url is None \
+            else   [ "some" ] + self.embedded_url.to_str_fields()
+        embed_tweet = [ "none" ] if self.embedded_tweet is None \
+            else      [ "some" ] + self.embedded_tweet.to_str_fields()
+
+        return core + url_card + embed_tweet
+
 
 
     @classmethod
@@ -84,12 +102,12 @@ class TweetBody:
         url_card, next_start = \
             UrlCard.from_str_fields(fields, start + 4) \
             if fields[start + 3].lower() == "some" \
-            else None, start + 4
+            else (None, start + 4)
 
         embed_tweet, next_next_start = \
             TweetBody.from_str_fields(fields, next_start + 1) \
             if fields[next_start].lower() == "some" \
-            else None, next_start + 1
+            else (None, next_start + 1)
 
         return TweetBody(id, author, msg, url_card, embed_tweet), next_next_start
 
@@ -120,9 +138,103 @@ class TweetEnvelope:
         # Re-order this to be a bit more amenable to the layout in this project
         local_date = dateparser.parse(fields[0])
         utc_date   = dateparser.parse(fields[1])
-        tweet, _   = TweetBody.from_str_fields(fields, 2)
+        # the third field is time-zone difference which we ignore
+        tweet, _   = TweetBody.from_str_fields(fields, 3)
 
         return TweetEnvelope(utc_date=utc_date, local_date=local_date, tweet=tweet)
+
+    def __str__(self):
+        # Figure out the timezone string
+        diff = self.utc_date - self.local_date
+        hours =
+
+        # Then write it out.
+        fields = [ self.local_date.isoformat(), self.utc_date.isoformat(), ""] + self.tweet.to_str_fields()
+        return "\t".join(fields)
+
+
+
+def tweet_files(dir):
+    """
+    Given a directory of tweet files broken down by category, so it's laid out in
+    the form <parent>/<cat-dir>/<screen-name>.<count> return the full list of files.
+    This is returned as a map from categories to a map of users to a list of user-files
+    which is sorted
+    """
+    result = dict()
+    sub_dirs = os.listdir(dir)
+    for sub_dir_name in sub_dirs:
+        sub_dir_name = dir + os.sep + sub_dir_name
+        sub_dir = Path(sub_dir_name)
+        if (not sub_dir.is_dir()) or sub_dir.name.startswith("."):
+            continue
+
+        sub_dir_contents = [sub_dir_name + os.sep + f for f in os.listdir(sub_dir_name)]
+        user_files = [f for f in sub_dir_contents if is_visible_file(f)]
+        if len(user_files) == 0:
+            continue
+
+        catgy_list = dict()
+        result[sub_dir.name] = catgy_list
+
+        user_files.sort(reverse=True)
+
+        current_user_batch = [user_files.pop()]
+        current_user_name  = screen_name_from_tweets_file(user_files[0])
+        while len(user_files) > 0:
+            f = user_files.pop()
+            p = Path(f)
+            fname = p.name
+
+            if fname.startswith(current_user_name):
+                current_user_batch.insert(0, f)
+            else:
+                catgy_list[current_user_name] = current_user_batch
+                current_user_name = screen_name_from_tweets_file(f)
+                current_user_batch = [f]
+
+        catgy_list[current_user_name] = current_user_batch
+
+    return result
+
+def min_ids (catuserfiles):
+    """
+    Given a map of category -> user -> user-files (As returned by <code>tweet_Files</code>)
+    return a map of category -> user -> tweet_id, with the oldest tweet-id for each user.
+    """
+    result = dict()
+    for caty, usermap in catuserfiles.items():
+        catmap = dict()
+        result[caty] = catmap
+        for user, userfiles in usermap.items():
+            with open (userfiles.pop(), "r") as f:
+                envelope = TweetEnvelope.from_str(f.readline())
+                print(str(envelope))
+                catmap[user] = envelope.tweet.tweet_id
+
+    return result
+
+def is_visible_file(f):
+    p = Path(f)
+    return p.is_file() and (not p.name.startswith("."))
+
+def screen_name_from_tweets_file (filename):
+    filename = Path(filename).name
+    pos = filename.rfind(".")
+    if pos < 0:
+        return filename
+    elif (filename[pos+1:]).isdigit():
+        return filename[:pos]
+    else:
+        raise ValueError("Invalid file name : " + filename)
+
+
+if __name__ == "__main__":
+    lst = tweet_files("/Users/bryanfeeney/Desktop/SpiderUpTest/")
+    mp  = min_ids(lst)
+
+    print(str(mp))
+
 
 
 
